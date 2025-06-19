632a5c95fd60e465258d9c1fd1f126e028d45f11
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-06-19 11:55:53 +0100

    testing deployment

--- admin/view/js/dashboard.js
+++
@@ -1,4 +0,0 @@
-async function dashboardActive() {
-    const userInfo = JSON.parse(sessionStorage.getItem('user'))
-    document.querySelector('h1 p span').innerHTML = `${userInfo.firstname}&nbsp;${userInfo.lastname}`
-}
